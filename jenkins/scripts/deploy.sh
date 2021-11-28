937aa7670ee89f5ec937740a9ad22ccdf878fca9
Zhikaizk <1902620@sit.singaporetech.edu.sg>
2021-11-29 00:24:13 +0800

    lala

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v C:\Users\gameb\Downloads\jenkins-phpunit-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\Users\gameb\Documents\GitHub\testing2\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
